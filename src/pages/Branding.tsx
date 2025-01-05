a84865efa827360340f3cbb7ae63c6349470a7cb
nscolin <157139542+nscolin@users.noreply.github.com>
2025-01-05 13:42:12 -0500

    Update Branding.tsx

--- src/pages/Branding.tsx
+++ src/pages/Branding.tsx
@@ -17,7 +17,7 @@ const Branding = () => {
           <div className="grid md:grid-cols-2 gap-12 items-center">
             <div>
               <img
-                src="https://images.unsplash.com/photo-1581091226825-a6a2a5aee158"
+                src="https://images.unsplash.com/photo-1657543348027-f304c0dae648?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Branding Services"
                 className="rounded-lg shadow-lg w-full"
               />
